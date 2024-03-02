24d195e75e4f7cd7290d278e7bb12a923ee80d97
liuyanxiang <307035586@qq.com>
2024-03-02 18:34:26 +0800

    前端引入antd开发框架

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
 
 // 引入APP.vue， 并将其填充到id=‘app’的div中（在public/index.html中）
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Antd).use(store).use(router).mount('#app')
