1963b4730c7d63b32c8ad9df533fc8742db6c562
liuyanxiang <307035586@qq.com>
2024-03-02 22:03:46 +0800

    前端引入ant图标框架

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Antd from 'ant-design-vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue'
 
 // 引入APP.vue， 并将其填充到id=‘app’的div中（在public/index.html中）
-createApp(App).use(Antd).use(store).use(router).mount('#app')
+// createApp(App).use(Antd).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(Antd).use(store).use(router).mount('#app')
+
+const icons = Icons
+for (const i in icons){
+    app.component(i, icons[i]);
+}
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -2,5 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
     <a-button>测试ddd</a-button>
+    <RightOutlined />
   </div>
 </template>
